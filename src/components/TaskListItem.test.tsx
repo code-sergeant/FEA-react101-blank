72d7db7adece61d9d4366b338bfd23c5d1d83b2a
Steven <32521113+code-sergeant@users.noreply.github.com>
2021-06-16 09:28:24 -0700

    Fix TaskListItem Test

--- src/components/TaskListItem.test.tsx
+++ src/components/TaskListItem.test.tsx
@@ -2,13 +2,19 @@ import * as React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { TaskListItem } from "./TaskListItem";
+import {
+  makeRandomDateString,
+  makeRandomTaskText
+} from "../helpers/testHelpers";
 
 describe("TaskListItem", () => {
   // TODO: Step 3
   it.skip("correctly renders a Task List Item", () => {
-    render(<TaskListItem title="Task 1" date={new Date(2021, 1, 1)} />);
+    const randomDate = makeRandomDateString();
+    const randomTaskText = makeRandomTaskText();
+    render(<TaskListItem title="Task 1" date={randomDate} />);
 
-    expect(screen.getByText("Task 1"));
-    expect(screen.getByText("Mon Feb 01 2021"));
+    expect(screen.getByText(randomTaskText));
+    expect(screen.getByText(randomDate));
   });
 });
